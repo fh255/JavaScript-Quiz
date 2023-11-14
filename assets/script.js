f7c6da4ff5cb8abaee76e78eeffbf9286a925cc3
fh255 <>
2023-11-14 10:02:41 +0000

    add simple JS code

--- assets/script.js
+++ assets/script.js
@@ -2,26 +2,48 @@ function calculateScore(){
     let totalQuestion= 5;
     let calculateScore=0;
 }
-// Check answers for question 1
-var question1 = document.querySelector('input[name="question1"]:checked');
-if (question1)
-    document.getElementById("feedback1").innerHTML = question1.value === "West-Indies" ? "Correct!" : "Incorrect. The correct answer is West Indies.";
-if (question1.value === "West-Indies") {
-    score++;
-}
-    else {
-    document.getElementById("feedback1").innerHTML = "Please select an answer.";
-}
-function correctAnswer(){
+function submitAnswers() {
+    var totalQuestions = 2;
+    var score = 0;
 
+    // Check answers for question 1
+    var q1 = document.querySelector('input[name="q1"]:checked');
+    if (q1) {
+        document.getElementById("feedback1").innerHTML = q1.value === "paris" ? "Correct!" : "Incorrect. The correct answer is Paris.";
+        if (q1.value === "paris") {
+            score++;
+        }
+    } else {
+        document.getElementById("feedback1").innerHTML = "Please select an answer.";
+    }
+
+    // Check answers for question 2
+    var q2 = document.querySelector('input[name="q2"]:checked');
+    if (q2) {
+        document.getElementById("feedback2").innerHTML = q2.value === "jupiter" ? "Correct!" : "Incorrect. The correct answer is Jupiter.";
+        if (q2.value === "jupiter") {
+            score++;
+        }
+    } else {
+        document.getElementById("feedback2").innerHTML = "Please select an answer.";
+    }
+
+    // Display the result
+    var resultElement = document.getElementById("result");
+    resultElement.innerHTML = "Your score is: " + score + " out of " + totalQuestions;
 }
 
-function incorrectAnswer(){
 
-}
-function incrementAnswer(){
+// Login Form
+function validateForm() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
-}
-function incrementWrongAnswer{
-
+    // Simple validation
+    if (username === "" || password === "") {
+        alert("Please enter both username and password.");
+    } else {
+        alert("Login successful! (Note: This is a dummy example)");
+        // You can add further logic, such as redirecting to another page or making an AJAX request.
+    }
 }
