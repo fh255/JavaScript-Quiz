8d162c3b081816e9ae3283660aa8a65558dba808
fh255 <>
2023-11-14 10:13:49 +0000

    add simple js

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,12 @@
 function calculateScore(){
     let totalQuestion= 5;
-    let calculateScore=0;
-}
-function submitAnswers() {
-    var totalQuestions = 2;
-    var score = 0;
+    let score=0;
 
     // Check answers for question 1
-    var q1 = document.querySelector('input[name="q1"]:checked');
-    if (q1) {
-        document.getElementById("feedback1").innerHTML = q1.value === "paris" ? "Correct!" : "Incorrect. The correct answer is Paris.";
-        if (q1.value === "paris") {
+    var question1 = document.querySelector('input[name="question1"]:checked');
+    if (question1) {
+        document.getElementById("feedback1").innerHTML = question1.value === "West-Indies" ? "Correct!" : "Incorrect. The correct answer is West Indies.";
+        if (question1.value === "West-Indies") {
             score++;
         }
     } else {
@@ -18,15 +14,7 @@ function submitAnswers() {
     }
 
     // Check answers for question 2
-    var q2 = document.querySelector('input[name="q2"]:checked');
-    if (q2) {
-        document.getElementById("feedback2").innerHTML = q2.value === "jupiter" ? "Correct!" : "Incorrect. The correct answer is Jupiter.";
-        if (q2.value === "jupiter") {
-            score++;
-        }
-    } else {
-        document.getElementById("feedback2").innerHTML = "Please select an answer.";
-    }
+    
 
     // Display the result
     var resultElement = document.getElementById("result");
@@ -43,7 +31,6 @@ function validateForm() {
     if (username === "" || password === "") {
         alert("Please enter both username and password.");
     } else {
-        alert("Login successful! (Note: This is a dummy example)");
-        // You can add further logic, such as redirecting to another page or making an AJAX request.
+        alert("Login successful!");
     }
 }
